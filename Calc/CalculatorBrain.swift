76f8c03a99e7a7e938e0637dacb10145db2f78e4
adminaccount <adminaccount@macminissu10.training.local>
2016-12-08 14:52:21 +0200

    Thu 5:52

---
+++ Calc/CalculatorBrain.swift
@@ -0,0 +1,22 @@
+//
+//  CalculatorBrain.swift
+//  Calc
+//
+//  Created by adminaccount on 12/7/16.
+//  Copyright © 2016 adminaccount. All rights reserved.
+//
+
+import Foundation
+
+internal class CalculatorBrain {
+    
+    internal func setOperand(operand: Double) { }
+    
+    internal func performOperation(symbol: String) { }
+    
+    internal var result: Double {
+        get {
+            return 0.0
+        }
+    }
+}
