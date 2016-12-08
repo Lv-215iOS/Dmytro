420cb1b3f472613a62e867fe12e1bb517b944ee9
adminaccount <adminaccount@MacMiniSSU10.local>
2016-12-08 21:35:15 +0200

    New_commit(Thur 9:35PM)

--- CalculatorBrain.swift
+++ CalculatorBrain.swift
@@ -8,39 +8,115 @@
 
 import Foundation
 
+//func multiply(op1: Double, op2: Double) -> Double {
+//    return op1 * op2
+//}
+//
+//func devide(op1: Double, op2: Double) -> Double {
+//    if op2 == 0 {
+//        return op2
+//    }
+//    return op1 / op2
+//}
+//
+//func add(op1: Double, op2: Double) -> Double {
+//    return op1 + op2
+//}
+//
+//func substract(op1: Double, op2: Double) -> Double {
+//    return op1 - op2
+//}
+
 class CalculatorBrain
 {
     private var accumulator = 0.0
     
-    func setOperand(operand: Double) {
+    private var internalProgram = [AnyObject]()
+    
+    func setOperand(operand: Double) { //getAlllabel
         accumulator = operand
+        internalProgram.append(operand as AnyObject)
     }
     
-    var  operations: Dictionary<String,Operation> = [
+    private var  operations: Dictionary<String,Operation> = [
         "π" : Operation.Constant(M_PI),
         "e" : Operation.Constant(M_E),
         "√" : Operation.UnaryOperation(sqrt),
-        "cos" : Operation.UnaryOperation(cos)
+        "cos" : Operation.UnaryOperation(cos),
+        "✕" : Operation.BinaryOperation({ $0 * $1}),
+        "=" : Operation.Equals,
+        "÷" : Operation.BinaryOperation({ $0 / $1}),
+        "+" : Operation.BinaryOperation({ $0 + $1}),
+        "-" : Operation.BinaryOperation({ $0 - $1}),
+        "+-" : Operation.UnaryOperation({ -$0 })
+        
     ]
     
-    enum Operation {
+    private enum Operation {
         case Constant(Double)
         case UnaryOperation ((Double) -> Double)
-        case BinaryOperation
+        case BinaryOperation ((Double, Double) -> Double)
         case Equals
         
     }
     
     func performOperation(symbol: String) {
+        internalProgram.append(symbol as AnyObject)
         if let operation = operations[symbol]{
             switch operation {
-            case .Constant(let value) : accumulator = value
-            case .UnaryOperation(let function) : accumulator = function(accumulator)
-            case .BinaryOperation : break
-            case .Equals : break
+            case .Constant(let value) :
+                accumulator = value
+            case .UnaryOperation(let function) :
+                accumulator = function(accumulator)
+            case .BinaryOperation(let function) :
+                pending = PendingDinaryOperationInfo(binaryFunction: function, firstOperand: accumulator)
+            case .Equals :
+                executePendingBinaryOperation()
                 
             }
         }
+    }
+    
+    private func executePendingBinaryOperation()
+    {
+        if pending != nil {
+            accumulator = pending!.binaryFunction(pending!.firstOperand, accumulator)
+            pending = nil
+        }
+    }
+    
+    private var pending: PendingDinaryOperationInfo?
+    
+    private struct  PendingDinaryOperationInfo {
+        var  binaryFunction : (Double, Double) -> Double
+        var  firstOperand : Double
+        
+    }
+    
+    typealias PropertyList = AnyObject
+    
+    var program: PropertyList {
+        get {
+            return internalProgram as CalculatorBrain.PropertyList
+        }
+        set {
+            clear()
+            if let arrayOfOps = newValue as? [AnyObject] {
+                for op in arrayOfOps {
+                    if let operand = op as? Double {
+                        setOperand(operand: operand)
+                    } else if let operation = op as? String {
+                        performOperation(symbol: operation)
+                    }
+                }
+            }
+        }
+    }
+    
+    func clear() {
+        accumulator = 0.0
+        pending = nil
+        internalProgram.removeAll()
     }
     
     var result: Double {
